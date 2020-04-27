06e4d83a2291c58ca230aadf43f8c309e8b7c728
Tom <tom@Maxs-MacBook-Pro.local>
2020-04-27 14:30:08 +1000

    Creating new functions

--- index.rb
+++ index.rb
@@ -1,4 +1,43 @@
 require 'colorize'
+require 'json' #gem install json
+
+puts "Welcome to the budgeting planner application. What is your name"
+name = gets.chomp
+logged_in = false;
+file = File.read('filehistory.json')
+data_array=JSON.parse(file)
+
+data_array.each do |element|
+  if element['name'] == name
+    puts "Welcome back".colorize(:color => :yellow)
+    logged_in = true
+  end
+end
+
+def create_new_entry()
+  while true
+    puts
+    puts "Enter your weekly income"
+    income = gets.chomp()
+    income_float = income.to_f
+    puts "Enter your weekly expenditure"
+    expenditure = gets.chomp()
+    expenditure_float = expenditure.to_f
+
+    begin
+      if  income_float == 0.0 || expenditure_float == 0.0
+        system 'clear'
+        raise ArgumentError, '"Please enter a numerical value only. Try again!"'
+      end
+      saving = income_float - expenditure_float
+      puts "Your weekly deficit/surplus was #{saving}"
+      break
+    rescue ArgumentError => e
+      puts "#{e.message}"
+    end
+  end
+end
+
 while true
   puts "=========================".colorize(:color => :light_blue)
   puts "Welcome to the main menu".colorize(:color => :light_blue)
@@ -15,34 +54,12 @@ while true
   puts "-View budget history- To view your previous data entries please type 'view' or 'v' into the terminal".colorize(:color => :green)
   puts
   puts "Enter 'E' or 'exit' at any time to quit the application.".colorize(:color => :green)
-  print    "● ".colorize(:color => :light_blue)
+  print    "● ".colorize(:color => :red)
 
   menu_input = gets.chomp.downcase
 
   if menu_input == 'n' || menu_input == 'new'
-
-    while true
-      puts
-      puts "Enter your weekly income"
-      income = gets.chomp()
-      income_float = income.to_f
-      puts "Enter your weekly expenditure"
-      expenditure = gets.chomp()
-      expenditure_float = expenditure.to_f
-
-      begin
-        if  income_float == 0.0 || expenditure_float == 0.0
-          system 'clear'
-          raise ArgumentError, '"Please enter a numerical value only. Try again!"'
-        end
-        saving = income_float - expenditure_float
-        puts "Your weekly deficit/surplus was #{saving}"
-        break
-      rescue ArgumentError => e
-        puts "#{e.message}"
-      end
-    end
-
+    create_new_entry()
     
   elsif menu_input == 'g' || menu_input == 'goal'
 
@@ -69,7 +86,17 @@ while true
     end
 
   elsif menu_input == 'v' || menu_input == 'view'
-    puts "boog"
+    file = File.read('filehistory.json')
+    data_array=JSON.parse(file)
+    data_array.each do |element|
+      puts element['name']
+    end
+      # data_hash['name']
+      # name = 
+      # puts name
+      # data_hash.keys
+    
+
   
 
   elsif menu_input == 'e' || menu_input == 'exit'
